dce0fdd1c6e49665526ae6466d5bc009526b6479
Oriolus <joker.n7@gmail.com>
2019-03-13 00:16:33 +0300

    adding config file

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+import logging
+import logging.config
+import yaml
 from flask_app import create_app
+
+
+with open('log.config.yaml', 'r') as log_conf:
+    config = yaml.safe_load(log_conf)
+    logging.config.dictConfig(config)
+
+logger = logging.getLogger(__name__)
+
+logger.debug('Message')
+logger.error('Error')
 
 app = create_app()
 
